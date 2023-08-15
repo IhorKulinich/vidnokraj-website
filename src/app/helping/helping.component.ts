0b5e21551f477b559ae4dcfcd61a611063f36a8a
IhorKulinich <78841262+IhorKulinich@users.noreply.github.com>
2023-08-15 03:42:51 +0300

    helping

--- src/app/helping/helping.component.ts
+++ src/app/helping/helping.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./helping.component.css']
 })
 export class HelpingComponent {
-  cards = [{img: '', title: '', p: '', ahref: ''}];
+  cards = [{img: 'auto(1)', title: 'Авто', p: 'швидке пересування', ahref: ''}, 
+          {img: 'drone(2)', title: 'дрони', p: 'наші очі у небі', ahref: ''}, 
+          {img: 'radio(3)', title: 'Зв`язок', p: 'розмовне питання', ahref: ''},
+          {img: 'ochi(4)', title: 'оптичні прилади', p: 'збільшуємо дальність обзору', ahref: ''},
+          {img: 'bpla(5)', title: 'бпла', p: 'захист у небі', ahref: ''},
+          {img: 'med(6)', title: 'тактична медицина', p: 'рятуємо життя', ahref: ''}];
   activeIndex!: string;
   focusItem(newItem: string) {
     this.activeIndex = newItem;
